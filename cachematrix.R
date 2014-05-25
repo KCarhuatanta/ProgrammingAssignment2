0bdae93cefce3099e1ea63a4893fc9d48a4e58f9
KCarhuatanta <krawczka@gmail.com>
2014-05-25 02:54:31 -0400

    Add cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix creates a matrix, caches it and also creates its inverse matrix and caches that.
 
+makeCacheMatrix <- function(x = matrix ()){
+  i<- NULL
+  set <- function (y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function()x
+  setinverse <- function(solve) i <<- inverse
+  getinverse <- function ()i
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the inverse of a function.  It first looks to see if the inverse is 
+## already cached so it can retrieve it if not it created the inverse matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+    i <- x$getinverse()
+    if(lis.null(i)){## this line is confusing to me, I could not find what lis.null does/refers to, please comment to me.
+      message("getting cached data")
+      return
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
+  }
+  
